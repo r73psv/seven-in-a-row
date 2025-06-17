f152dcc52de89091e3fed861a917bfd95659d84c
r73psv <42837708+r73psv@users.noreply.github.com>
2025-06-17 14:42:39 +0400

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+# Конфигурация проекта
+MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.abcd.mongodb.net/seveninarow?retryWrites=true&w=majority"
+JWT_SECRET_KEY = "your_secret_key_here"
+DEBUG = True
