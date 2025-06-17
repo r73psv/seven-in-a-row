92f2d431c92f6e6e540e78d6171c0968da623b2c
r73psv <42837708+r73psv@users.noreply.github.com>
2025-06-17 14:54:35 +0400

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,8 @@
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+export const register = (data) => api.post('/register', data);
+export const login = (data) => api.post('/login', data);
